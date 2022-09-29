32509d94a80816626735523236f6ae18c08ae202
Jorge Fernandez Gonzales <jorgefg34@gmail.com>
2022-09-29 16:00:06 -0400

    similacion de login y idea del home de cada usuario

--- Javascript/controller.js
+++ Javascript/controller.js
@@ -1,7 +1,8 @@
 
-
+const ID_USER_FIELD=document.getElementById("user");
 const ID_PASSWORD_FIELD=document.getElementById("pswd");
-const ID_DIV_WARNING= document.getElementById("warning-div");
+const ID_DIV_WARNING= document.getElementById("warningdiv");
+const ICON = document.getElementById("iconPswd");
 const USER = 'Admin';
 const PASSWORD= 'Admin123';
 export default class Controller {
@@ -16,19 +17,60 @@ export default class Controller {
 
 
       verificationLogin(){
-        
-        console.log(document.getElementById("user").value);
-        //console.log((document.getElementById("user").textContent.length));
-      /*  if.localCompare(USER)){
+
+      console.log(ID_USER_FIELD.value);
+      console.log(ID_PASSWORD_FIELD.value);
+      if(ID_USER_FIELD.value === USER && ID_PASSWORD_FIELD.value === PASSWORD ){
+        console.log("los dato son coinciden");
+        location.assign("./src/home/home.html");
+        this.clean();
+
+       }else if(ID_USER_FIELD.value.trim() == "" && ID_PASSWORD_FIELD.value.trim()==""){
+          ID_USER_FIELD.style.borderColor = "red";
+          ID_PASSWORD_FIELD.style.borderColor = "red";
          
-         // location.assign("../Javascript/src/login/login.html");
-         console.log("es igual");
-        
-        } else{
-          console.log(" no es igual");
-        }*/
+       }else{
+        ID_DIV_WARNING.style.display = "block";
+        this.clean();
+       }
 
       }
+
+      showPassword(){
+         
+          const eye= "fa-eye";
+          const slasheye="fa-eye-slash";
+          if(ICON.classList.contains("fa-eye")){
+           this.enableShowPSWD();
+          }else if(ICON.classList.contains("fa-eye-slash")){
+            this.disableShowPSWD();
+          }
+
+      }
+
+      enableShowPSWD(){
+        ICON.classList.remove("fa-eye");
+        ICON.classList.add("fa-eye-slash");
+        ID_PASSWORD_FIELD.type = "text";
+
+      }
+
+      disableShowPSWD(){
+        ICON.classList.remove("fa-eye-slash");
+        ICON.classList.add("fa-eye");
+        ID_PASSWORD_FIELD.type = "password";
+
+      } 
+
+      restoreFields(){
+
+        ID_DIV_WARNING.style.display = "none";
+        ID_USER_FIELD.style.borderColor = "#0d6efd";
+        ID_PASSWORD_FIELD.style.borderColor = "#0d6efd";
+       
+      }
+
+
     
 
 }
